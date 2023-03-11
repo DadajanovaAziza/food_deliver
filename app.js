ad96ad1afb5dac637b2a94a4f0cf7d43156c12b5
Azizka <125365158+DadajanovaAziza@users.noreply.github.com>
2023-03-11 17:37:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+VANTA.BIRDS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x0,
+    color1: 0x4d0169,
+    color2: 0xd6b0e6,
+    birdSize: 2.20,
+    speedLimit: 6.00,
+    separation: 54.00,
+    alignment: 100.00,
+    cohesion: 1.00
+})
